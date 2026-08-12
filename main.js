2061b6bb878387c8b25fedeb87c125212e84b87e
Jader-code <jader.ramos@escola.pr.gov.br>
2026-08-11 21:29:57 -0300

    ajustes 3

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const botoes = document.querySelector('.parametro-senha__botao');
 let tamanhoSenha = 12;
 
 botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1 ) {
-        tamanhoSenha = tamanhoSenha - 1
+        tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContext = tamanhoSenha;
     geraSenha();
